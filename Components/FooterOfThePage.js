bf28fc2692a29f518fea68f921a17f14cd6d161a
Daunted00 <jmhogeveen@student.edu.nl>
2024-12-07 12:58:40 +0100

    Scaling, opdrachtenV1.0, deleting tailwindCSS

---
+++ Components/FooterOfThePage.js
@@ -0,0 +1,21 @@
+class FooterOfThePage extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <footer>
+                <div class="grid-container">
+                    <div class="grid-item">
+                    <img class="littleLogo" src="../images/Logo.png" alt="Website-Logo">
+                    </div>
+                    <div class="grid-item">Bedankt voor het bezoeken van mijn website.</div>
+                    <div class="grid-item"> &#169 2024 Mijn zelf gemaakte website gemaakt door Jochem Hogeveen</div>
+                </div>
+            </footer>
+        `;
+    }
+}
+
+customElements.define('page-footer', FooterOfThePage);
--- Components/NavigationBar.js
+++ Components/NavigationBar.js
@@ -5,15 +5,15 @@ class navigationbar extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-            <nav>
+             <nav>
                 <div>
-                    <a class="return" href="../sitemap.html"><img src="../fontIcons/arrow-left-solid.svg" alt="Return"/>Terug</a>
+                    <a class="return" href="../sitemap.html"><img src="../fontIcons/arrow-left-solid-white.svg" alt="Terug-Icoon"/>Terug</a>
                 </div>
                 <div>
-                    <img class="logo" src="../images/smiley.png" alt="smiley.jpg">
+                    <img class="logo" src="../images/Logo.png" alt="Website-Logo.jpg">
                 </div>
                 <div>
-                     <a class="menu" href="../sitemap.html">Menu<img src="../fontIcons/bars-solid.svg" alt="menu"/></a>
+                    <a class="menu" href="../sitemap.html">Menu<img src="../fontIcons/bars-solid-white.svg" alt="menu-Icoon"/></a>
                 </div>
             </nav>
         `;
